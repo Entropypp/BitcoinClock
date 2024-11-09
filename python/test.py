b555e9c5688828d41342055d2e221ded6a3b7d1a
unknown <ajanks+entropypp@gmail.com>
2024-11-09 11:01:00 -0800

    added project

--- python/test.py
+++ python/test.py
@@ -32,10 +32,9 @@ try:
 	logging.info("Drawing on the image...")
 	image = Image.new('1', (epd.height, epd.width), 255)
 	draw = ImageDraw.Draw(image)
-	epd.init_fast()
-	epd.displayPartBaseImage(epd.getbuffer(image))
 	for i in range(75000,75123,22):
 		epd.init_fast()
+		epd.displayPartBaseImage(epd.getbuffer(image))
 		draw.rectangle((0, 0, epd.height, epd.width), fill = 255)
 		draw.text((5, 5),"${}".format(i), font = font, fill = 0)
 		#epd.display_fast(epd.getbuffer(image.rotate(180)))
