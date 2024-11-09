6425eb2b38fafecd8ed401e83ba865c0d21520f1
unknown <ajanks+entropypp@gmail.com>
2024-11-08 21:58:13 -0800

    notabs

--- python/test.py
+++ python/test.py
@@ -22,22 +22,22 @@ try:
     logging.info("init and Clear")
     epd.init()
     epd.Clear(0xFF)
-	
+    
     # Drawing on the image
     font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
     font24 = ImageFont.truetype(os.path.join(picdir, 'Janky.ttf'), 24)
     
-	logging.info("E-paper refresh")
+    logging.info("E-paper refresh")
 
-	logging.info("E-paper refreshes quickly")
-	epd.init_fast()
-	logging.info("1.Drawing on the image...")
-	image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
-	draw = ImageDraw.Draw(image)
+    logging.info("E-paper refreshes quickly")
+    epd.init_fast()
+    logging.info("1.Drawing on the image...")
+    image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
+    draw = ImageDraw.Draw(image)
     for i in range(75000,75123,25):
-		draw.text((110, 90),i, font = font24, fill = 0)
-		epd.display_fast(epd.getbuffer(image))
-		time.sleep(2)
+        draw.text((110, 90),i, font = font24, fill = 0)
+        epd.display_fast(epd.getbuffer(image))
+        time.sleep(2)
         
     logging.info("Clear...")
     epd.init()
