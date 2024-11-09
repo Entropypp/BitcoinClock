02701b33d8cd117de49e40e2b19cce4aeb5e82e7
unknown <ajanks+entropypp@gmail.com>
2024-11-09 12:00:27 -0800

    notabs

--- python/tick_tock.py
+++ python/tick_tock.py
@@ -39,7 +39,7 @@ try:
 
 	epd.init_fast()
 	draw.rectangle((0, 0, epd.height, epd.width), fill = 255)
-	draw.text((5, 5),"${}".format(btcusd), font = font, fill = 0)
+	draw.text((5, 5),"${}".format(btc_usd_price), font = font, fill = 0)
 	epd.display_fast(epd.getbuffer(image.rotate(180)))
 	epd.sleep()
 
