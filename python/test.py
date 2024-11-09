a39eb3a75c05067d60ee243cdfb710be48f031f6
unknown <ajanks+entropypp@gmail.com>
2024-11-08 23:37:06 -0800

    notabs

--- python/test.py
+++ python/test.py
@@ -38,7 +38,7 @@ try:
     for i in range(75000,75123,22):
         draw.rectangle((0, 0, epd.height, epd.width), fill = 255)
         draw.text((5, 5),"${}".format(i), font = font, fill = 0)
-        epd.displayPartial(epd.getbuffer(image))
+        epd.displayPartial(epd.getbuffer(image.rotate(180))
         time.sleep(10)
         
     logging.info("Clear...")
