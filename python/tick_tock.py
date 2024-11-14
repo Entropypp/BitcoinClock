3bb16ed3a82a4a7bd279ff48f1f03f209b1c6ee7
unknown <ajanks+entropypp@gmail.com>
2024-11-13 16:44:58 -0800

    First release

--- python/tick_tock.py
+++ python/tick_tock.py
@@ -15,10 +15,7 @@ import traceback
 logging.basicConfig(level=logging.DEBUG)
 
 import json 
-import requests 
- 
-import argparse
-
+import requests
   
 def get_btc_usd():
 	try:
@@ -31,7 +28,7 @@ def get_btc_usd():
 
 def get_font(font_name,max_width,text_string):
 	font_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'fonts')
-	font_size = 10
+	font_size = 5
 	width = 0
 	while width<max_width:
 		font = ImageFont.truetype(os.path.join(font_dir, font_name), font_size)
@@ -58,41 +55,6 @@ def epaper_btc_price():
 		return False
 	return True
 
-def get_image_filename(img_name):
-	image_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'images')
-	return os.path.join(image_dir, img_name)
-
-def address_is_pwned(btc_address,sats_expected):
-	return True
-
-def send_pwned_alert():
-	return True
-	
-def epaper_pwned():
-	try:
-		epd = epd2in13_V4.EPD()
-		btc_string = get_btc_usd()
-		epd.init_fast()
-		image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-		bmp = Image.open(get_image_filename('pwned.bmp'))
-		image.paste(bmp, (0,0))    
-		epd.display_fast(epd.getbuffer(image.rotate(180)))
-		epd.sleep()
-		
-	except Exception as e:
-		logging.info(e).sats
-		epd2in13_V4.epdconfig.module_exit(cleanup=True)
-		return False
-		
-	return True
 
 ####Main
-parser = argparse.ArgumentParser()
-parser.add_argument('-a','--address',default='',help="Bitcoin address to watch")
-parser.add_argument('-s','--sats',type=int, default=0,help="Expected sats ")
-args = parser.parse_args()
-if args.address != '' and  address_is_pwned(args.address,args.sats):
-	epaper_pwned()
-	send_pwned_alert()
-else: 
-	epaper_btc_price()
+epaper_btc_price()
